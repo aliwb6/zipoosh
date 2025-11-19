21bdea09de844c0674056324b8ca23c8717e6b34
Alireza <124519949+aliwb6@users.noreply.github.com>
2025-11-19 23:54:43 +0330

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+app.use(())
 const connectDB = require('./config/db');
 
 // Load environment variables
